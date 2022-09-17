9444aa4257d833f04d22b34355bf122d9dd769d0
larissaslff <110000521+larissaslff@users.noreply.github.com>
2022-09-17 10:39:56 -0300

    implementação da segurança

---
+++ src/main/java/com/projeto/integrador/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.projeto.integrador.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .httpBasic()
+        .and()
+        .authorizeHttpRequests()
+        .anyRequest().authenticated()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        .and()
+        .csrf().disable();
+
+    }
+}
